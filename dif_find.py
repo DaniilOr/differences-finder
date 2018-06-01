eabef6961126027cff565673fdf6f596f5c58b83
Daniil <36066254+DaniilOr@users.noreply.github.com>
2018-06-01 18:16:22 +0600

    Update dif_find.py

--- dif_find.py
+++ dif_find.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw 
+#img1 - your first image
 img1 = Image.open("img1.jpg") 
+#img2 - your second image
 img2 = Image.open("img2.jpg") 
 p_set1 = img1.load() 
 p_set2 = img2.load() 
@@ -11,10 +13,12 @@ for i in range(width):
 		r=p_set1[i,j][0]-p_set2[i,j][0]
 		g=p_set1[i,j][1]-p_set2[i,j][1] 
 		b=p_set1[i,j][2]-p_set2[i,j][2]
+		#Constants may be changed, it depends on the image
 		if(abs(r)>80  or abs(g)>80  or abs(b)>80 ):
+			#every 7th point, which has difference of colors greater then given constant will be highlited 
 			if(i % 7==0 and j%7==0):
 
 				ImageDraw.Draw(ans).ellipse((i, j, i + 7, j + 7), fill = (0, 0, 0))
 
-			
+#the result is saved as dif.jpg file			
 ans.save("dif.jpg", "JPEG")
